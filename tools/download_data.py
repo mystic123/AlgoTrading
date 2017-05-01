c80ac338d278f58b1347758e456512f4798a8212
Pawel Kapica <pawel.kapica@gmail.com>
2017-05-01 18:07:41 +0200

    added multithreading and cleaned code

--- tools/download_data.py
+++ tools/download_data.py
@@ -1,49 +1,83 @@
-#!/usr/local/bin/python
+#!/usr/bin/env python
+
+import Queue
+import argparse
 import datetime as dt
-from dateutil.relativedelta import relativedelta
-import sys
 import io
-from zipfile import ZipFile
-from io import BytesIO, FileIO, StringIO
-from urllib import urlopen
+import multiprocessing
 import os
+import sys
+from io import BytesIO
+from threading import Thread
+from urllib import urlopen
+from zipfile import ZipFile
 
-if len(sys.argv) < 4:
-	print 'Arguments: pair date-from(Y-m) date-to(Y-m) dir\nExample: ' + sys.argv[0] + ' EUR_USD 2015-01 2016-01 data'
-	sys.exit()
+from dateutil.relativedelta import relativedelta
 
-if len(sys.argv) >= 4:
-    pair = sys.argv[1].upper()
-    dateFrom = dt.datetime.strptime(sys.argv[2], '%Y-%m')
-    dateTo = dt.datetime.strptime(sys.argv[3], '%Y-%m')
+URL = 'http://ratedata.gaincapital.com/'
+num_worker_threads = multiprocessing.cpu_count()
+urls_queue = Queue.Queue()
+save_dir = './'
 
-path = '.'
 
-if len(sys.argv) >= 5:
-	path = sys.argv[4]
+def _get_parser():
+    parser = argparse.ArgumentParser(description='Tool for downlaoding forex data from http://ratedata.gaincapital.com')
+    parser.add_argument('pair', type=str, help='currency pair with underscore eg. EUR_USD')
+    parser.add_argument('date_from', type=str, help='date from in format Y-m eg. 2015-01')
+    parser.add_argument('date_to', type=str, default='', help='date to in format Y-m eg. 2016-01')
+    parser.add_argument('-o', '--dir', type=str, default='./', help='dir where save files')
+    return parser
 
-if not os.path.exists(path):
-	os.makedirs(path)
 
-source = 'http://ratedata.gaincapital.com/'
-date = dateFrom
-while date < dateTo:
-	downloadFile = pair + '_Week'
-	datePrefix = date.strftime('%Y-%m-')
-	for week in xrange(1,6):
-		url = source + date.strftime('%Y/%m%%20%B/') + downloadFile + str(week) + '.zip'
-		try:
-			resp = urlopen(url)
-			if resp.getcode() == 200:
-				sys.stderr.write('Downloading: ' + url + '\n')
-				with ZipFile(BytesIO(resp.read())) as zfile:
-					fileName = datePrefix + downloadFile + str(week) + '.csv'
-					with io.FileIO(os.path.join(path,fileName), 'w') as newfile:
-						if len(zfile.namelist()) > 1:
-							sys.stderr.write('More than one file in archive: ' + fileName)
-						zipcontent = zfile.read(zfile.namelist()[0])
-						newfile.write(zipcontent)
-		except Exception as e:
-			print type(e)
-			print e
-	date += relativedelta(months=1)
+def worker():
+    while not urls_queue.empty():
+        url, date_prefix, download_file, week = urls_queue.get()
+        try:
+            resp = urlopen(url)
+            if resp.getcode() == 200:
+                sys.stderr.write('Downloading: ' + url + '\n')
+                with ZipFile(BytesIO(resp.read())) as zfile:
+                    file_name = date_prefix + download_file + str(week) + '.csv'
+                    with io.FileIO(os.path.join(save_dir, file_name), 'w') as new_file:
+                        if len(zfile.namelist()) > 1:
+                            sys.stderr.write('More than one file in archive: {}\n'.format(file_name))
+                        zip_content = zfile.read(zfile.namelist()[0])
+                        new_file.write(zip_content)
+        except Exception as e:
+            print type(e)
+            print e
+
+        urls_queue.task_done()
+
+
+def main():
+    parser = _get_parser()
+    args = parser.parse_args()
+
+    global save_dir
+    save_dir = args.dir
+
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+
+    date_from = dt.datetime.strptime(args.date_from, '%Y-%m')
+    date_to = dt.datetime.strptime(args.date_to, '%Y-%m')
+
+    date = date_from
+
+    while date < date_to:
+        download_file = args.pair + '_Week'
+        date_prefix = date.strftime('%Y-%m-')
+        for week in xrange(1, 6):
+            url = URL + date.strftime('%Y/%m%%20%B/') + download_file + str(week) + '.zip'
+            urls_queue.put([url, date_prefix, download_file, week])
+        date += relativedelta(months=1)
+
+    for i in range(num_worker_threads):
+        t = Thread(target=worker)
+        t.start()
+
+    urls_queue.join()
+
+if __name__ == '__main__':
+    main()
